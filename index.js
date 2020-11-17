a46706233f6067091ff52484d9717f0fa1ad67c9
gean_duv <geanca25081@gmail.com>
2020-11-17 18:18:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// ------------------------------slideshow 
+let slideIndex = 0;
+carrusel();
+
+function carrusel (){
+let i;
+let slides = document.getElementsByClassName('slide-img-items');
+for(i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+}
+
+slideIndex++;
+
+if(slideIndex > slides.length){
+    slideIndex = 1;
+}
+slides[slideIndex - 1].style.display = "block";
+setTimeout(carrusel, 2000);
+}
