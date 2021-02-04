c2fd8fc8d04acb192e3015d4719a990e94e3fc42
gean_duv <geanca25081@gmail.com>
2021-02-04 09:26:23 -0500

    arregle el menu,  pero tngo un bug de que debe presionar 2 veces el icono para que se abra el menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// al presionar el boton de ver detalles se le agrega una clase  que sera la encargada de mostrar el frente
+// muestra los detalles de cada card
 function showDeta(){
      let boton = document.getElementById('down')
     //  boton.style.display = "none"
@@ -10,4 +10,27 @@ function showDeta(){
      {
           boton.style.display = "block" 
      } 
+}
+
+
+
+
+// logica del icono de menu
+let menuIcono = document.getElementById('myNav');
+
+function openNav(){
+     // console.log('si est oy llegadno1')
+     if(menuIcono.style.display === "none")
+     {
+          // console.log('si estoy llegadno2')
+         return menuIcono.style.display = "block" 
+     } else {
+          closeNav();
+     }
+     
+}
+function closeNav(){
+     // let closeIcono = document.getElementById('myNav')
+
+          return menuIcono.style.display = "none";
 }
