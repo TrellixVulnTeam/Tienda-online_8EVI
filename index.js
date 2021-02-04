584367f849dcad6fae56575efcfe8d8a42c1a245
gean_duv <geanca25081@gmail.com>
2021-02-03 19:27:21 -0500

    quite el flip de las cartas y le agregue un down effect, nuevo bug solo la primera carta aparece y se esconde, no importa que carta active el boton

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // al presionar el boton de ver detalles se le agrega una clase  que sera la encargada de mostrar el frente
-function mostrarDetalles(){
-    let botonDetalles = document.querySelector('.btn-detalle-card');
+function showDeta(){
+     let boton = document.getElementById('down')
+    //  boton.style.display = "none"
+     if(boton.style.display === "block")
+     {
+         return boton.style.display = "none" 
+     } 
+     else (boton.style.display === "none")
+     {
+          boton.style.display = "block" 
+     } 
 }
-
-function ocultarDestalles(){}
