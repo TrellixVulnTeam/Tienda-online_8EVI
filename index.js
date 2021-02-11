fef83908c6b1ac40dcbbe29399f2ad7b07fc65fc
jcapelo <jhonnathanp27@gmail.com>
2021-02-10 21:42:43 -0500

    MOD CArrito

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
 // logica del icono de menu
-let menuIcono = document.getElementById('myNav');
-
-function openNav(){
-     // console.log('si est oy llegadno1')
-     if(menuIcono.style.display === "none")
-     {
-          // console.log('si estoy llegadno2')
-         return menuIcono.style.display = "block" 
-     } else {
-          closeNav();
-     }
-     
-}
-let closeNav = () =>  menuIcono.style.display = "none";
-
-
-// muestra los detalles de cada card
-function showDeta() {
-  let boton = document.getElementById("down");
-  //  boton.style.display = "none"
-  if (boton.style.display === "block") {
-    return (boton.style.display = "none");
-  } else boton.style.display === "none";
-  {
-    boton.style.display = "block";
-  }
-}
-
-// logica del icono de menu
 let menuIcono = document.getElementById("myNav");
 
 function openNav() {
@@ -64,21 +35,25 @@ function getLocalStorage(key) {
   return val;
 }
 
-function addProductos(categoria) {
-  const nombre = "Tarro tupperware 3ml 111121212";
+function addProductos(data) {
+  let url = window.location.href;
+  url = new URL(url);
+  let c = url.searchParams.get("categoria");
+  console.log("categoria", c);
 
-  let html = ` <div class="card w3-card card-detalles-main  " style="border-radius:5px;">
+  const nombre = "Tarro tupperware 3ml 111121212";
+  //ombre: "tuperware 1", img: "https://picsum.photos/200/300", precio: "2.6", detalle:
+
+  data.map((item) => {
+    let html = ` <div class="card w3-card card-detalles-main  " style="border-radius:5px;">
 
      <figure class="card-img">
-       <img src="./img/jpegs/tarros/img-jpgs/tarro-calaveras-colores.jpg" alt="" srcset="">
-       <figcaption class="w3-center">${nombre}</figcaption>
+       <img src="${item.img}" alt="" srcset="">
+       <figcaption class="w3-center">${item.nombre}</figcaption>
      </figure>
      <!-- seccion trasera de la tarjta -->
      <div id="down" class="down-descript">
-       <span class="down-description-span">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quam sint
-         eos accusantium cumque nobis exercitationem quae officia repellat? Aliquam vel exercitationem incidunt in
-         tenetur fugiat commodi optio consequuntur laudantium!
-         Fugiat distinctio, dolores esse ipsa recusandae </span>
+       <span class="down-description-span">${item.detalle}</span>
      </div>
 
 
@@ -87,13 +62,13 @@ function addProductos(categoria) {
          <button id="mostrar-detalles" class="btn-detalles-card" onclick="showDeta()">Ver detalles</button>
          <div class="contador">
            <span>Cantidad:</span>
-           <input id="${nombre}" style="max-width: 50px; text-align: center;" placeholder="0" type="number" min="1" value=1 >
+           <input id="${item.nombre}" style="max-width: 50px; text-align: center;" placeholder="0" type="number" min="1" value=1 >
 
          </div>
        </div>
 
        <button id="dsd" class="botones " style="background-color: transparent;"
-         onclick="agregarCarrito( document.getElementById('${nombre}') )">
+         onclick="agregarCarrito( document.getElementById('${item.nombre}') )">
 
          <span class="w3-btn detalles-btn">
            Agregar al carrito
@@ -105,13 +80,26 @@ function addProductos(categoria) {
    </div>
    `;
 
-  document.getElementById("container-productos").innerHTML += html;
+    document.getElementById("container-productos").innerHTML += html;
+  });
 }
 
 function isCargada() {
-  const ssds = window.location.href;
+  //usage:
+  readTextFile("./productos.json", function (text) {
+    console.log(text);
+    var data = JSON.parse(text);
+    console.log(data);
+  });
+}
 
-  console.log("location", ssds);
-
-  addProductos();
+function readTextFile(file, callback) {
+  fetch("./productos.json")
+    .then((response) => response.json())
+    .then((data) => {
+      addProductos(data);
+      console.log("data", data);
+      return data;
+    })
+    .catch((error) => console.log("error", error));
 }
